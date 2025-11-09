06473a8a79e4d92273a1c460a973dd1e6dd3d1d8
Landilux834 <u202314275@upc.edu.pe>
2025-11-09 17:39:56 -0500

    agregando el usuariolistar

---
+++ FrontMoneyMentor/src/app/components/usuario/usuariolistar/usuariolistar.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { RouterLink } from "@angular/router";
+import { Usuario } from "../../../models/Usuario";
+import { UsuarioService } from "../../../services/usuario-service";
+
+@Component({
+  selector: 'app-usuariolistar',
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, MatIconModule],
+  templateUrl: './usuariolistar.html',
+  styleUrls: ['./usuariolistar.css'],
+})
+export class usuariolistar implements OnInit {
+    dataSource: MatTableDataSource<Usuario> = new MatTableDataSource();
+    displayedColumns: string[] = ['c1', 'c2', 'c3' ];
+    constructor(private uS:UsuarioService) {}
+    ngOnInit(): void {
+        this.uS.list().subscribe((data) => {
+            console.log('usuarios recibidos', data);
+            this.dataSource = new MatTableDataSource(data);
+        });
+        //this.uS.getList().subscribe((data) => {
+        //    this.dataSource = new MatTableDataSource(data);
+        //});
+    }
+}
