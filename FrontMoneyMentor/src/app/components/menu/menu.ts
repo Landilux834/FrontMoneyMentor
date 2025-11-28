30e4a9b845559e3876b072bbb2a7f2ea831a2c92
mau <mauanto007@gmail.com>
2025-11-28 01:03:56 -0500

    reporte balance

--- FrontMoneyMentor/src/app/components/menu/menu.ts
+++ FrontMoneyMentor/src/app/components/menu/menu.ts
@@ -39,6 +39,6 @@ export class Menu {
   }
 
   isTester() {
-    return this.role?.includes('TESTER') || this.role?.includes('USER') || this.role?.includes('CLIENT');
+    return this.role?.includes('TESTER') || this.role?.includes('CLIENT');
   }
 }
